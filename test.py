38c7ccf45ee6ba068e123fa31e1279d838d9c501
Hyler <admchyler@fire.ca.gov>
2018-06-06 18:12:15 -0700

    first update

--- test.py
+++ test.py
@@ -3,7 +3,7 @@
 import time
 from splinter import Browser
 executable_path = {'executable_path':'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'}
-browser = Browser(driver_name='chrome')
+browser = Browser('iexplorer')
 browser.visit('http://www2.jblearning.com/my-account/login')
 browser.find_by_id('Content_C015_ctl00_ctl00_UserName_ctl00_ctl00_textBox_write').first.fill('nick.laff@fire.ca.gov')
 browser.find_by_id('Content_C015_ctl00_ctl00_Password_ctl00_ctl00_textBox_write').first.fill('Calfire1!')
